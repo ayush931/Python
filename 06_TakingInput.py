3083f30734ff2a4825e4c733a90a249bf70f80fd
ayush931 <ayushkumar9315983@gmail.com>
2024-03-07 15:09:57 +0530

    Taking Input

---
+++ 06_TakingInput.py
@@ -0,0 +1,9 @@
+a = input ("Enter your name: ")
+
+print ("My name is", a)
+
+x = input ("Enter first number: ") # It will take input as a string.
+y = input ("Enter second number: ")
+
+print (x + y) # It will concatenate the string.
+print (int(x) + int(y))
