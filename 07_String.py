f20ae673ea324366ba3e538aa2852b3de86df316
ayush931 <ayushkumar9315983@gmail.com>
2024-03-07 18:43:59 +0530

    String

---
+++ 07_String.py
@@ -0,0 +1,31 @@
+name = "Ayush"
+friend = "Aman"
+anotherFriend = "Ankit"
+
+apple = 'He said, "I want to eat an apple."' # It will print the string with double quote.
+print (apple)
+
+print ("Hello, " + name)
+
+# Triple single / double quote will make the multiline string
+
+apple = '''He said,
+Hii Ayush
+Hey I am good
+"I want to eat an apple."
+'''
+
+print (apple)
+
+print (name[0]) # It will give the first element of the string. Also called 0th index. It is like an array of characters.
+print (name[1]) 
+print (name[2]) 
+print (name[3]) 
+print (name[4])
+
+print ('\n')
+
+print ("Lets use a for loop \n")
+
+for character in name:
+    print (character)
