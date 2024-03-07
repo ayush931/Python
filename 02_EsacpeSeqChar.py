23de982e6db6311a7f31e31cf2931f724881540e
ayush931 <ayushkumar9315983@gmail.com>
2024-03-07 13:55:19 +0530

    Escape Sequence Character

---
+++ 02_EsacpeSeqChar.py
@@ -0,0 +1,24 @@
+print ("Hey I am a good boy \n and my friends are also good.") 
+
+# "\n" is a escape sequence new line character. After "\n", all the characters will be printed into a new line.
+
+'''
+    This is the way to write the multi-line comment.
+'''
+
+"""
+    Another Way to write multi-line comment.
+"""
+
+print ("Hey I am a good boy \n and my \"friends\" are also good.") # It will write friend in double quote.
+
+# If we have to write a double quoted word in a double quoted string then, we have to use \"Character\" double quote escape sequence character.
+
+print ("Hey I am a good boy \n and my 'friends' are also good.")
+
+# We can use single quote character in a double quote string.
+
+print ("Hey", 6, 7, sep="~") # Sep is called separater. It will separate the input by given sep.
+
+print ("Ayush", 5, 6, end="009\n") # "end" will print the given data at the end of the print statement.
+print ("Aman")
