5eaeddc99827951b179e0661bc25cd240c1c61b0
ayush931 <ayushkumar9315983@gmail.com>
2024-03-07 14:37:45 +0530

    Operators

---
+++ 04_Operators.py
@@ -0,0 +1,17 @@
+print (15 + 5)
+print (15 - 5)
+print (15 * 5)
+print (15 / 5)
+print (15 % 5) # It will give remainder.
+print (15 // 5) # It is called Floor division operator. It will give an integer value.
+print (5 ** 3) # It will give 5 x 5 x 5.
+
+n = 15
+m = 6
+
+print ("Addition of", n, "and", m, "is", n + m)
+print ("Substraction of", n, "and", m, "is", n - m)
+print ("Multiplication of", n, "and", m, "is", n * m)
+print ("Division of", n, "and", m, "is", n / m)
+print ("Modulus of", n, "and", m, "is", n % m)
+print ("Floor Division of", n, "and", m, "is", n // m)
