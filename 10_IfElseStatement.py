f0c533b5cc76418178fb93a4e3177ec863fb8a9c
ayush931 <ayushkumar9315983@gmail.com>
2024-03-09 13:34:52 +0530

    If Else Statement

---
+++ 10_IfElseStatement.py
@@ -0,0 +1,24 @@
+a = int(input("Enter your age: "))
+print ("Your age is:", a)
+
+if (a > 18):
+    print ("You can drive")
+else:
+    print ("You cannot drive")
+print ("Hii") # It will print because it is not the part of the if else statement.
+# Conditional operators
+# >, <, >=, <=, ==, !=
+
+print (a > 18)
+print (a <= 18)
+print (a == 18)
+print (a != 18)
+
+applePrice = 210
+budget = 200
+
+if (applePrice <= budget):
+    print ("Alexa, add 1 kg Apples to the cart.")
+else:
+    print ("Alexa, do not add Apples to the cart.")
+    
